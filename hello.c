d3e424206c5992c641f45c3d1484ff164fd84217
Cuukel_15 <sow.pape-bilaly@ugb.edu.sn>
2022-12-30 11:23:09 +0000

    Challenge 1

--- hello.c
+++ hello.c
@@ -1,10 +1,7 @@
 #include "cuukel15/lib.h"
-#include <stdlib.h>
-#include <string.h>
 
 int main() {
-    char *hy = (char*) malloc((strlen("Hello World !") + 1) * sizeof(char));
-    strcpy(hy, "Hello World !");
+    char hy[14] = "Hello World !";
     for(short i = 0; hy[i] != '\0'; i++)
         PrintChar(hy[i]);
     NextLine();
