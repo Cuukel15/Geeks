6fb58d0d0e268e7698de815692be2cd72b7ea972
Cuukel_15 <sow.pape-bilaly@ugb.edu.sn>
2022-12-31 14:14:44 +0000

    Update

--- cuukel15/lib.c
+++ cuukel15/lib.c
@@ -1,14 +1,18 @@
 #include "lib.h"
+#include <stdio.h>
 
 void NextLine() {
-    printf("\n");
+    PrintChar('\n');
 }
 
 void PrintChar(char c) {
     printf("%c", c);
 }
 
-void PrintString(char str[]) {
-    for(unsigned i = 0; str[i] != '\0'; i++)
-        PrintChar(str[i]);
+void PrintString(char *str) {
+    if(!str)
+        PrintString("ERROR: no memory space for this string");
+    else
+        for(unsigned i = 0; str[i] != '\0'; i++)
+            PrintChar(str[i]);
 }
--- cuukel15/lib.h
+++ cuukel15/lib.h
@@ -1,10 +1,8 @@
 #if !defined(LIB_HEADER_FILE_H)
 #define LIB_HEADER_FILE_H
 
-#include <stdio.h>
-
 void NextLine();
 void PrintChar(char);
-void PrintString(char[]);
+void PrintString(char*);
 
 #endif // CUUKEL'S LIBRARY
